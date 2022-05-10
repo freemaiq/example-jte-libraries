077ce4f843b817572233120c4e677b68a8b11d52
maiquelet <migueloliverrodas@gmail.com>
2022-05-10 11:41:23 +0200

    Refactor on release step

--- libraries/maven/steps/release.groovy
+++ libraries/maven/steps/release.groovy
@@ -5,20 +5,17 @@ void call(){
         
         // Changing chart's version and appVersion
         chart_content = readYaml file: 'int/Chart.yaml'
-        sh "echo 'CURRENT CHART VERSION: ${chart_content.version}'"
-        sh "echo 'CURRENT APP VERSION: ${chart_content.appVersion}'"
+        println "CURRENT CHART VERSION: ${chart_content.version}"
+        println "CURRENT APP VERSION: ${chart_content.appVersion}"
         chart_content.appVersion = POM.version
         chart_content.version = POM.version
         writeYaml file: 'int/Chart.yaml', overwrite: true, data: chart_content
-        sh "echo 'NEW CHART VERSION: ${chart_content.version}'"
-        sh "echo 'NEW APP VERSION: ${chart_content.appVersion}'"
         
         // Changing microservice.version from values.yaml
-        sh "cat int/values.yaml"
         values_content = readYaml file: 'int/values.yaml'
         values_content.microservice.version=chart_content.appVersion
         writeYaml file: 'int/values.yaml', overwrite: true, data: values_content
-        sh "cat int/values.yaml"
+        println "NEW APP VERSION: ${values_content.microservice.version}"
         
         // Commit and push
         sh "git config user.email 'jenkins' && git config user.name 'moliver-ext@aubay.es'"
